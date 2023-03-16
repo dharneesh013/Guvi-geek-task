b7c4b878048e99f26fb8eb33c76491411fbccb9d
Dharneesh B <dharneesh.ec20@bitsathy.ac.in>
2023-03-16 15:20:05 +0530

    Add files via upload

---
+++ js/register.js
@@ -0,0 +1,22 @@
+function submitData(){
+    $(document).ready(function(){
+      var data = {
+        name: $("#name").val(),
+        username: $("#username").val(),
+        password: $("#password").val(),
+        action: $("#action").val(),
+      };
+
+      $.ajax({
+        url: '../php/function.php',
+        type: 'post',
+        data: data,
+        success:function(response){
+          alert(response);
+          if(response == "Login Successful"){
+            window.location.reload();
+          }
+        }
+      });
+    });
+  }
